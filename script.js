ace2de992c02048ad1194bcbf80666fb87d4a0dd
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-18 21:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here
+// //your code here
+const btn = document.querySelector("button");
+const main =  document.querySelector("main");
+const text = document.querySelector("p");
 
+main.id = "app";
+main.className = "day";
+
+text.id = "ptag";
+text.innerText = "I am an AccioJob student, ready to become an awesome full stack developer.";
+
+btn.id = "swap";
+btn.className = "button_day";
+
+const swapTheme = () => {
+	// console.log("hello");
+	main.classList.toggle("night");
+	btn.classList.toggle("button_night");
+}
+
+btn.addEventListener('click', swapTheme);
